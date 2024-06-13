a56ac53c13391d72bd1b1247389a993ced30fabf
sandy <sandy.ali.basha@gmail.com>
2024-06-13 16:12:03 +0300

    update

--- js/script.js
+++ js/script.js
@@ -138,3 +138,58 @@ document.addEventListener("DOMContentLoaded", function () {
     $("body").toggleClass("darkMode");
   });
 });
+
+$(document).ready(function () {
+	// Preloader
+	var preloaderFadeOutTime = 500;
+	function hidePreloader() {
+		var preloader = $(".spinner-wrapper");
+		preloader.fadeOut(preloaderFadeOutTime);
+	}
+	hidePreloader();
+});
+
+var cursor = $(".cursor"),
+    follower = $(".cursor-follower");
+
+var posX = 0,
+    posY = 0;
+
+var mouseX = 0,
+    mouseY = 0;
+
+TweenMax.to({}, 0.016, {
+  repeat: -1,
+  onRepeat: function() {
+    posX += (mouseX - posX) / 9;
+    posY += (mouseY - posY) / 9;
+    
+    TweenMax.set(follower, {
+        css: {    
+        left: posX - 12,
+        top: posY - 12
+        }
+    });
+    
+    TweenMax.set(cursor, {
+        css: {    
+        left: mouseX,
+        top: mouseY
+        }
+    });
+  }
+});
+
+$(document).on("mousemove", function(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+$("button, a").on("mouseenter", function() {
+    cursor.addClass("active");
+    follower.addClass("active");
+});
+$("button, a").on("mouseleave", function() {
+    cursor.removeClass("active");
+    follower.removeClass("active");
+});
